241f9328223b76d8a67262177cf251108153b9af
Paulo Vitor <paulovitor@MacBook-Pro-de-Paulo.local>
2021-05-19 23:25:28 -0300

    :sparkles: Adicionando estoque service

--- src/NerdStore.Catalogo.Data/Repository/ProdutoRepository.cs
+++ src/NerdStore.Catalogo.Data/Repository/ProdutoRepository.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using NerdStore.Catalogo.Domain;
 using NerdStore.Core.Data;
 
